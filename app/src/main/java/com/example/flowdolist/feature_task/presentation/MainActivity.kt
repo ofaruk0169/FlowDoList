be94c06b974cf65c5abc7a3aa886d212a23a73ab
Omare Faruk <ofaruk0169@gmail.com>
2024-04-18 10:28:09 +0100

    just deleted comments for DI that were not used

--- app/src/main/java/com/example/flowdolist/feature_task/presentation/MainActivity.kt
+++ app/src/main/java/com/example/flowdolist/feature_task/presentation/MainActivity.kt
@@ -3,24 +3,13 @@ package com.example.flowdolist.feature_task.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import androidx.room.Room
-import com.example.flowdolist.feature_task.data.data_source.TaskDatabase
-import com.example.flowdolist.TaskScreen
-
-import com.example.flowdolist.feature_task.presentation.add_edit_task.AddEditTaskEvent
 import com.example.flowdolist.feature_task.presentation.add_edit_task.AddEditTaskScreen
 import com.example.flowdolist.feature_task.presentation.tasks.TasksScreen
 import com.example.flowdolist.feature_task.presentation.util.Screen
@@ -29,21 +18,6 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-
-    //Dagger Hilt start
-    /*
-    private val db by lazy {
-
-        Room.databaseBuilder(
-            applicationContext,
-            TaskDatabase::class.java,
-            "tasks.db"
-        ).build()
-    }*/
-
-
-    //Dagger Hilt end
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
