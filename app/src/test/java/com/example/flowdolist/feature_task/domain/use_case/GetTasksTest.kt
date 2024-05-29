39dcc132f588bfc44269fa4813310248eeb4db56
Omare Faruk <ofaruk0169@gmail.com>
2024-05-29 22:00:23 +0100

    fixed my mistake without understanding the Task object or just forgetting because i havent coded in so long

--- app/src/test/java/com/example/flowdolist/feature_task/domain/use_case/GetTasksTest.kt
+++ app/src/test/java/com/example/flowdolist/feature_task/domain/use_case/GetTasksTest.kt
@@ -2,6 +2,8 @@ package com.example.flowdolist.feature_task.domain.use_case
 
 import com.example.flowdolist.feature_task.data.repository.FakeTaskRepository
 import com.example.flowdolist.feature_task.data.repository.TaskRepositoryImpl
+import com.example.flowdolist.feature_task.domain.model.Task
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.*
 import org.junit.Before
 
@@ -14,8 +16,25 @@ class GetTasksTest {
     fun setUp() {
         fakeRepository = FakeTaskRepository()
         getTasks = GetTasks(fakeRepository)
-    }
 
+        val tasksToInsert = mutableListOf<Task>()
+        ('a'..'z').forEachIndexed { index, c ->
+            tasksToInsert.add(
+                Task(
+                    taskName = c.toString(),
+                    taskDescription = c.toString(),
+                    taskCreatedAt = index.toLong(),
+                    color = index
+                )
+            )
+        }
+        tasksToInsert.shuffle()
+        runBlocking {
+            tasksToInsert.forEach { fakeRepository.upsertTask(it)}
+        }
+
+
+    }
 }
 
 //we have made a fake repository do the rest some other day.
