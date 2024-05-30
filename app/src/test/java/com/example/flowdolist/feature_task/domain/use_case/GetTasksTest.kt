49fe10ca34215a9dde1dd6997782a46357863309
Omare Faruk <ofaruk0169@gmail.com>
2024-05-30 21:12:09 +0100

    test cases initial pass for GetTasksTest

--- app/src/test/java/com/example/flowdolist/feature_task/domain/use_case/GetTasksTest.kt
+++ app/src/test/java/com/example/flowdolist/feature_task/domain/use_case/GetTasksTest.kt
@@ -3,8 +3,12 @@ package com.example.flowdolist.feature_task.domain.use_case
 import com.example.flowdolist.feature_task.data.repository.FakeTaskRepository
 import com.example.flowdolist.feature_task.data.repository.TaskRepositoryImpl
 import com.example.flowdolist.feature_task.domain.model.Task
+import com.example.flowdolist.feature_task.domain.util.OrderType
+import com.example.flowdolist.feature_task.domain.util.TaskOrder
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
-import org.junit.Assert.*
+
 import org.junit.Before
 import org.junit.Test
 
@@ -36,8 +40,57 @@ class GetTasksTest {
     }
 
     @Test
-    fun `Order tasks by title ascending, correct order`() {
+    fun `Order tasks by title ascending, correct order`() = runBlocking {
+        val tasks = getTasks(TaskOrder.Title(OrderType.Ascending)).first()
 
+        for(i in 0..tasks.size - 2) {
+            assertThat(tasks[i].taskName).isLessThan(tasks[i+1].taskName)
+        }
+    }
+
+    @Test
+    fun `Order tasks by title descending, correct order`() = runBlocking {
+        val tasks = getTasks(TaskOrder.Title(OrderType.Descending)).first()
+
+        for(i in 0..tasks.size - 2) {
+            assertThat(tasks[i].taskName).isGreaterThan(tasks[i+1].taskName)
+        }
+    }
+
+    @Test
+    fun `Order tasks by date ascending, correct order`() = runBlocking {
+        val tasks = getTasks(TaskOrder.Date(OrderType.Ascending)).first()
+
+        for(i in 0..tasks.size - 2) {
+            assertThat(tasks[i].taskCreatedAt).isLessThan(tasks[i+1].taskCreatedAt)
+        }
+    }
+
+    @Test
+    fun `Order tasks by date descending, correct order`() = runBlocking {
+        val tasks = getTasks(TaskOrder.Date(OrderType.Descending)).first()
+
+        for(i in 0..tasks.size - 2) {
+            assertThat(tasks[i].taskCreatedAt).isGreaterThan(tasks[i+1].taskCreatedAt)
+        }
+    }
+
+    @Test
+    fun `Order tasks by color ascending, correct order`() = runBlocking {
+        val tasks = getTasks(TaskOrder.Color(OrderType.Ascending)).first()
+
+        for(i in 0..tasks.size - 2) {
+            assertThat(tasks[i].color).isLessThan(tasks[i+1].color)
+        }
+    }
+
+    @Test
+    fun `Order tasks by color descending, correct order`() = runBlocking {
+        val tasks = getTasks(TaskOrder.Color(OrderType.Descending)).first()
+
+        for(i in 0..tasks.size - 2) {
+            assertThat(tasks[i].color).isGreaterThan(tasks[i+1].color)
+        }
     }
 }
 
