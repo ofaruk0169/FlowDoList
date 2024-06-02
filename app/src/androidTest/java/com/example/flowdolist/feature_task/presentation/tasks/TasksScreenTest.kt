3f4189d571ff5df052f6e1fe94b710ec15acc387
Omare Faruk <ofaruk0169@gmail.com>
2024-06-02 20:07:53 +0100

    started with the file

---
+++ app/src/androidTest/java/com/example/flowdolist/feature_task/presentation/tasks/TasksScreenTest.kt
@@ -0,0 +1,11 @@
+package com.example.flowdolist.feature_task.presentation.tasks
+
+import org.junit.Assert.*
+
+class TasksScreenTest {
+
+    //I made the class and I know where instrumented tests go, been driving all day
+    //so will work on this soon inshallah.
+
+
+}
