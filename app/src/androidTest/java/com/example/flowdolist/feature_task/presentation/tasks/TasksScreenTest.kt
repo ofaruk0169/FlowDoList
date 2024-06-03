18e3f3eb19307d36eb09d0610387368df1c96231
Omare Faruk <ofaruk0169@gmail.com>
2024-06-03 20:50:14 +0100

    initial hilt cases used for di

--- app/src/androidTest/java/com/example/flowdolist/feature_task/presentation/tasks/TasksScreenTest.kt
+++ app/src/androidTest/java/com/example/flowdolist/feature_task/presentation/tasks/TasksScreenTest.kt
@@ -1,7 +1,13 @@
 package com.example.flowdolist.feature_task.presentation.tasks
 
+import com.example.flowdolist.di.AppModule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
 import org.junit.Assert.*
 
+
+@HiltAndroidTest
+@UninstallModules(AppModule::class)
 class TasksScreenTest {
 
     //I made the class and I know where instrumented tests go, been driving all day
---
+++ app/src/test/java/com/example/flowdolist/di/TestAppModule.kt
@@ -0,0 +1,49 @@
+package com.example.flowdolist.di
+
+import android.app.Application
+import androidx.room.Room
+import com.example.flowdolist.feature_task.data.data_source.TaskDatabase
+import com.example.flowdolist.feature_task.data.repository.TaskRepositoryImpl
+import com.example.flowdolist.feature_task.domain.repository.TaskRepository
+import com.example.flowdolist.feature_task.domain.use_case.AddTask
+import com.example.flowdolist.feature_task.domain.use_case.DeleteTask
+import com.example.flowdolist.feature_task.domain.use_case.GetTask
+import com.example.flowdolist.feature_task.domain.use_case.GetTasks
+import com.example.flowdolist.feature_task.domain.use_case.TaskUseCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TestAppModule {
+
+    //Instance of our database (Room)
+    @Provides
+    @Singleton
+    fun provideTaskDatabase(app: Application): TaskDatabase {
+        return Room.inMemoryDatabaseBuilder(
+            app,
+            TaskDatabase::class.java,
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTaskRepository(db: TaskDatabase): TaskRepository {
+        return TaskRepositoryImpl(db.dao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideTaskUseCases(repository: TaskRepository): TaskUseCases {
+        return TaskUseCases(
+            getTasks = GetTasks(repository),
+            deleteTasks = DeleteTask(repository),
+            addTask = AddTask(repository),
+            getTask = GetTask(repository)
+        )
+    }
+}
