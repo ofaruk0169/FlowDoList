f3c8e16dcac48d3647009ae177bb8ff427d8d9a9
Omare Faruk <ofaruk0169@gmail.com>
2024-06-27 21:20:59 +0100

    note to self

--- app/src/main/java/com/example/flowdolist/feature_task/domain/use_case/AddTask.kt
+++ app/src/main/java/com/example/flowdolist/feature_task/domain/use_case/AddTask.kt
@@ -1,5 +1,6 @@
 package com.example.flowdolist.feature_task.domain.use_case
 
+import android.util.Log
 import com.example.flowdolist.feature_task.domain.model.InvalidTaskException
 import com.example.flowdolist.feature_task.domain.model.Task
 import com.example.flowdolist.feature_task.domain.repository.TaskRepository
@@ -11,12 +12,15 @@ class AddTask(
     @Throws(InvalidTaskException::class)
     suspend operator fun invoke(task: Task) {
         if(task.taskName.isBlank()){
+            Log.e("TaskError", "The title of the task cannot be empty")
             throw InvalidTaskException("The title of the task cannot be empty")
         }
         if(task.taskDescription.isBlank()) {
+            Log.e("TaskError", "The description of the task cannot be empty")
             throw InvalidTaskException("The description of the task cannot be empty")
         }
         repository.upsertTask(task)
     }
+    //woudl be more useful to have a snackbar here to display to the user this action cannot be performed, log cat isn't good enough.
 }
 
