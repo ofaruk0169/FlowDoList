f1c9355c14d78691bfad32d4c4cb271e3e6d48b3
Omare Faruk <ofaruk0169@gmail.com>
2024-07-18 22:16:42 +0100

    added test depencency properly

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,25 +61,28 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-
-
-
-
-
-    //test implementation is for local unit tests, dont need emulator to run
-    //testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
     implementation ("androidx.compose.material:material-icons-extended:1.6.5")
 
+    //Room
+    val room_version = "2.6.1"
+    implementation ("androidx.room:room-ktx:$room_version")
+    kapt ("androidx.room:room-compiler:$room_version")
+
+    //Dagger - Hilt
+    implementation("com.google.dagger:hilt-android:2.50")
+    kapt("com.google.dagger:hilt-android-compiler:2.50")
+    kapt("com.google.dagger:hilt-compiler:2.50")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     //local unit tests (might need to change this and instrumentation below)
-
-    implementation ("androidx.test:core:1.4.0")
+    testImplementation ("androidx.test:core:1.4.0")
     testImplementation("junit:junit:4.13.2")
     testImplementation("androidx.arch.core:core-testing:2.2.0")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.1")
@@ -87,7 +90,6 @@ dependencies {
     testImplementation ("com.squareup.okhttp3:mockwebserver:4.9.1")
     testImplementation("io.mockk:mockk:1.10.5")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.1.0-alpha04")
-
 
     //Instrumentation tests
     androidTestImplementation ("com.google.dagger:hilt-android-testing:2.37")
@@ -101,23 +103,6 @@ dependencies {
     androidTestImplementation ("com.squareup.okhttp3:mockwebserver:4.9.1")
     androidTestImplementation ("io.mockk:mockk-android:1.10.5")
     androidTestImplementation ("androidx.test:runner:1.4.0")
-
-
-
-
-
-    //Room
-    val room_version = "2.6.1"
-    implementation ("androidx.room:room-ktx:$room_version")
-    //this kapt is for annotation processing
-    kapt ("androidx.room:room-compiler:$room_version")
-
-    //Hilt
-    implementation("com.google.dagger:hilt-android:2.50")
-    kapt("com.google.dagger:hilt-android-compiler:2.50")
-    kapt("com.google.dagger:hilt-compiler:2.50")
-    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-
 }
 
 kapt {
