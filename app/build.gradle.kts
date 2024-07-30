00161b5a4c319e2c6f889ffcbaf0af1f086df093
Omare Faruk <ofaruk0169@gmail.com>
2024-07-30 22:45:43 +0100

    branched

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,18 +53,18 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+    implementation("androidx.activity:activity-compose:1.9.1")
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation ("androidx.compose.material:material-icons-extended:1.6.5")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation ("androidx.compose.material:material-icons-extended:1.6.8")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.06.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
@@ -81,27 +81,28 @@ dependencies {
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     //local unit tests (might need to change this and instrumentation below)
-    testImplementation ("androidx.test:core:1.4.0")
+    testImplementation ("androidx.test:core:1.6.1")
     testImplementation("junit:junit:4.13.2")
     testImplementation("androidx.arch.core:core-testing:2.2.0")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.1")
     testImplementation ("com.google.truth:truth:1.1.3")
     testImplementation ("com.squareup.okhttp3:mockwebserver:4.9.1")
     testImplementation("io.mockk:mockk:1.10.5")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.1.0-alpha04")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.8")
+
 
     //Instrumentation tests
     androidTestImplementation ("com.google.dagger:hilt-android-testing:2.37")
-    kaptAndroidTest ("com.google.dagger:hilt-android-compiler:2.37")
+    kaptAndroidTest ("com.google.dagger:hilt-android-compiler:2.50") //96?
     androidTestImplementation ("junit:junit:4.13.2")
     androidTestImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.1")
-    androidTestImplementation ("androidx.arch.core:core-testing:2.1.0")
+    androidTestImplementation ("androidx.arch.core:core-testing:2.2.0")
     androidTestImplementation ("com.google.truth:truth:1.1.3")
-    androidTestImplementation ("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation ("androidx.test:core-ktx:1.4.0")
+    androidTestImplementation ("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation ("androidx.test:core-ktx:1.6.1")
     androidTestImplementation ("com.squareup.okhttp3:mockwebserver:4.9.1")
     androidTestImplementation ("io.mockk:mockk-android:1.10.5")
-    androidTestImplementation ("androidx.test:runner:1.4.0")
+    androidTestImplementation ("androidx.test:runner:1.6.1")
 }
 
 kapt {
--- app/src/androidTest/java/com/example/flowdolist/feature_task/presentation/tasks/TasksScreenTest.kt
+++ app/src/androidTest/java/com/example/flowdolist/feature_task/presentation/tasks/TasksScreenTest.kt
@@ -6,7 +6,6 @@ import com.example.flowdolist.feature_task.presentation.MainActivity
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
-import org.junit.Assert.*
 import org.junit.Rule
 
 
