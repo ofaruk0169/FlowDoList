524b6209c1eda6fe5cedfec778e8cf8c85b02c76
Omare Faruk <ofaruk0169@gmail.com>
2024-07-30 22:36:44 +0100

    switching branches

--- app/src/androidTest/java/com/example/flowdolist/feature_task/presentation/tasks/TasksScreenTest.kt
+++ app/src/androidTest/java/com/example/flowdolist/feature_task/presentation/tasks/TasksScreenTest.kt
@@ -1,13 +1,25 @@
 package com.example.flowdolist.feature_task.presentation.tasks
 
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.flowdolist.core.util.TestTags
 import com.example.flowdolist.di.AppModule
 import com.example.flowdolist.feature_task.presentation.MainActivity
+import com.example.flowdolist.feature_task.presentation.util.Screen
+import com.example.flowdolist.ui.theme.FlowDoListTheme
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 
 @HiltAndroidTest
@@ -19,4 +31,32 @@ class TasksScreenTest {
 
     @get:Rule(order = 1)
     val composeRule = createAndroidComposeRule<MainActivity>()
+
+    @Before
+    fun setUp() {
+        hiltRule.inject()
+        composeRule.setContent {
+            val navController = rememberNavController()
+            //we need to assign our theme here. This is potentially my problem.
+           FlowDoListTheme {
+               NavHost(
+                   navController = navController,
+                   startDestination = Screen.TasksScreen.route
+               ) {
+                   composable(route = Screen.TasksScreen.route) {
+                       TasksScreen(navController = navController)
+
+
+                   }
+               }
+           }
+
+        }
+    }
+    @Test
+    fun clickToggleOrderSection_isVisible() {
+        composeRule.onNodeWithTag(TestTags.ORDER_SECTION).assertDoesNotExist()
+        composeRule.onNodeWithContentDescription("Sort").performClick()
+        composeRule.onNodeWithTag(TestTags.ORDER_SECTION).assertIsDisplayed()
+    }
 }
---
+++ app/src/main/java/com/example/flowdolist/core/util/TestTags.kt
@@ -0,0 +1,6 @@
+package com.example.flowdolist.core.util
+
+object TestTags {
+
+    const val ORDER_SECTION = "ORDER_SECTION"
+}
