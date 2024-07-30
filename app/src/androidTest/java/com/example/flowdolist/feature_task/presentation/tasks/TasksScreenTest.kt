b61fd57d38023e4dea523a99f17440d40a59432b
Omare Faruk <ofaruk0169@gmail.com>
2024-07-30 21:39:29 +0100

    thank god for chatgpt

--- app/src/androidTest/java/com/example/flowdolist/feature_task/presentation/tasks/TasksScreenTest.kt
+++ app/src/androidTest/java/com/example/flowdolist/feature_task/presentation/tasks/TasksScreenTest.kt
@@ -1,14 +1,22 @@
 package com.example.flowdolist.feature_task.presentation.tasks
 
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import com.example.flowdolist.di.AppModule
+import com.example.flowdolist.feature_task.presentation.MainActivity
+import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
 import org.junit.Assert.*
+import org.junit.Rule
 
 
 @HiltAndroidTest
 @UninstallModules(AppModule::class)
 class TasksScreenTest {
 
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
 
+    @get:Rule(order = 1)
+    val composeRule = createAndroidComposeRule<MainActivity>()
 }
